6f20106dbca4e44af38abc1ea5065be4bcbb7011
Zach Shipley <zach@zachshipley.com>
2013-05-25 21:09:51 -0500

    Grunt 0.4 support

--- tasks/grunt-contrib-nodefy.js
+++ tasks/grunt-contrib-nodefy.js
@@ -1,40 +1,54 @@
 module.exports = function(grunt) {
+
 	'use strict';
+
 
 	var path = require('path');
 	var async = require('async');
-	var util = require('./util.js');
+	var amd = require('grunt-lib-amd');
 	var nodefy = require('nodefy');
-	var _ = grunt.utils._;
 
 
-	grunt.registerTask('nodefy', 'Converts AMD modules to CommonJS modules', function() {
-		var config = grunt.config.get(this.name);
+	grunt.registerMultiTask('nodefy', 'Converts AMD modules to CommonJS modules', function() {
 		var done = this.async();
+		var rjsconfig = amd.loadConfig(grunt.config.get('requirejs'));
 
-		config.include = util.expand(config.include);
-		config.exclude = util.expand(config.exclude);
-		var files = _.difference(config.include, config.exclude);
+		grunt.verbose.writeln('Loaded RequireJS config:');
+		grunt.verbose.writeln(JSON.stringify(rjsconfig, false, 4));
 
-		util.loadConfig(grunt.config.get('requirejs'), function(err, rjsconfig) {
-			grunt.log.writeln('Converting modules to node.js:');
-			async.forEachSeries(files, function(file, callback) {
+		grunt.log.writeln('Converting modules to CommonJS format:');
+		async.forEachSeries(this.files, function(files, callback) {
+			var src = files.src.filter(function(filepath) {
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			});
+
+			async.forEachSeries(src, function(file, callback) {
+
 				nodefy.convert(file, function(err, result) {
 					if (err) {
-						grunt.log.write(file);
+						grunt.log.write(src);
 						grunt.log.error();
 						grunt.log.verbose(err);
 						return;
 					}
 
-					var dest = path.resolve(config.dest, util.fileToModuleName(file, rjsconfig));
+					var dest = path.resolve(files.dest, amd.fileToModuleName(file, rjsconfig) + '.js');
 					grunt.log.writeln(file + ' -> ' + path.relative(process.cwd(), dest));
 					grunt.file.write(dest, result, 'utf-8');
 					callback(null);
 				});
+
 			}, function() {
-				done();
+				callback(null);
 			});
+
+		}, function() {
+			done();
 		});
 	});
 
